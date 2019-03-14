c5bf48d0994c8c8a36a57ce5604663d876c832bb
Andreas Pohl <andreaspohl89@gmail.com>
2019-03-14 15:24:57 +0100

    Bugfixes

--- FuncyAutoScale/RegisterServer.cs
+++ FuncyAutoScale/RegisterServer.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
@@ -12,9 +13,10 @@ namespace FuncyAutoScale
 {
     public static class RegisterServer
     {
-        [FunctionName("RegisterServer")]
+        [FunctionName("RegisterServer2")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [Table("GameserverList")] CloudTable ServerTable,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
